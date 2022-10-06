3f415b2253ce709ee85584ca558750b2e87befda
bogdantudose14 <47989491+bogdantudose14@users.noreply.github.com>
2022-10-06 16:12:48 +0300

    Update assessments.js

--- Lab#2/js/assessments.js
+++ Lab#2/js/assessments.js
@@ -1,3 +1,6 @@
+
+//******************************************** ASSESSMENT #3 ****************************************************
+
 const arrayConcat = (numbersList) => {
   let result = [];
 
@@ -16,3 +19,31 @@ const arrayConcat = (numbersList) => {
 
 // console.log(arrayConcat(123));
 console.log(arrayConcat("123,1,23,345"));
+
+
+//******************************************** ASSESSMENT #4 ****************************************************
+
+const text = "Why does Javascript seem so weird";
+
+const relativeFrequencies = (textParam) => {
+  let result = {};
+  const letterArray = textParam.trim();
+
+  // Javascript equivalent of forEach => for..of
+  // we can use this control structure if we do not need the indexes
+
+  // .trim() is a string method and it removes whitespace from
+  //  both ends of a string and returns a new string, without modifying the
+  //  original string
+
+  for (let letter of letterArray) {
+    if (letter in result) {
+      result[letter]++;
+    } else result[letter] = 1;
+  }
+
+  return result;
+};
+
+console.log(relativeFrequencies(text));
+
