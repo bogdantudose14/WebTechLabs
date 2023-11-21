490c0d20b9bb1c1f51ea2a7903376d2285af4238
bogdantudose14 <tbogdan97@yahoo.com>
2023-11-21 12:08:30 +0200

    Added Lab#8

---
+++ Lab#8/README.md
@@ -0,0 +1,6 @@
+### Steps for running the express app
+
+1. Move inside de REST folder.
+2. Run the ```npm install``` command => This will look inside package.json and install the dependencies from there.
+3. Run the ```npm start``` command => mapped inside package.json, under "scripts". 
+  ```npm start``` is now equivalent to ```nodemon ./app.js```.
---
+++ Lab#8/REST/Database/moviesDatabase.js
@@ -0,0 +1,28 @@
+const movies = [
+  {
+    id: 1,
+    title: 'Black Panther: Wakanda Forever',
+    genre: 'action',
+    year: 2022,
+  },
+  {
+    id: 2,
+    title: 'My Neighbor Totoro',
+    genre: 'animation',
+    year: 1988,
+  },
+  {
+    id: 3,
+    title: 'Soul',
+    genre: 'animation',
+    year: 2020,
+  },
+  {
+    id: 4,
+    title: 'Hamilton',
+    genre: 'biography',
+    year: 2020,
+  },
+];
+
+export { movies };
