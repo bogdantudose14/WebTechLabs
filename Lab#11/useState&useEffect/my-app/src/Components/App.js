9209768ce871a9a065e8f8e06af0b00d00f9c4d5
bogdantudose14 <tbogdan97@yahoo.com>
2023-12-22 11:47:05 +0200

    Added React useState&useEffect Lab + FullStack app model

--- Lab#11/useState&useEffect/my-app/src/Components/App.js
+++ Lab#11/useState&useEffect/my-app/src/Components/App.js
@@ -1,19 +1,19 @@
-import "./App.css";
-import Counter from "./Counter";
-import Title from "./Title";
+import './App.css'
+import Counter from './Counter'
+import Title from './Title'
 
 // it is recommended to have a sepparate .css file for every component file
 // by convention, React components should be named with capital letter (HTML components have lowercase naming)
-function App() {
+function App () {
   return (
     // 'className' attribute is equivalent to the 'class' attribute for HTML tags
     // the JSX code returned in a component function should return ONLY ONE thing
     // ! tip => wrap everything up in a tag or return a single component
-    <div className="container">
+    <div className='container'>
       <Counter />
       <Title />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- Lab#11/useState&useEffect/my-app/src/Components/Title.js
+++ Lab#11/useState&useEffect/my-app/src/Components/Title.js
@@ -1,26 +1,26 @@
-import React, { useState, useEffect } from "react";
-import "./Title.css";
+import React, { useState, useEffect } from 'react'
+import './Title.css'
 
-function Title() {
-  const [value, setValue] = useState("");
+function Title () {
+  const [value, setValue] = useState('abc')
 
   // in the second parameter of useEffect, provide the dependencies array
   // when a dependency changes, the callback (first parameter) runs again
   useEffect(() => {
-    document.title = `${value}`;
-  }, [value]);
+    document.title = `${value}`
+  }, [value])
 
   return (
-    <div className="title">
+    <div className='title'>
       <p>Change page title:</p>
       {/* e.target.value gives us the current input content 
       Setting a value attribute isn't necessary, but it's usually a good idea.
       Putting a value attribute makes the input controlled, which means that the
       value in the input will always exactly correspond to what's in the React
       state, which will make the program's logic easier to reason about. */}
-      <input value={value} onChange={(e) => setValue(e.target.value)} />
+      <input value={value} onChange={e => setValue(e.target.value)} />
     </div>
-  );
+  )
 }
 
-export default Title;
+export default Title
