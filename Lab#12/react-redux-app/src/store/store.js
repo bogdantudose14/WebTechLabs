4242d54fa62cd5675ca6283a677d18bba3ae7713
bogdantudose14 <tbogdan97@yahoo.com>
2024-01-09 13:57:07 +0200

    Renamed Lab#11 subfolders due to possible faulty installation of node packages

--- Lab#12/react-redux-app/src/store/store.js
+++ Lab#12/react-redux-app/src/store/store.js
@@ -1,11 +1,11 @@
-import { createStore, combineReducers } from "redux";
+import { createStore } from 'redux'
 
-const initialState = { counter: 0, showCounter: true };
+const initialState = { counter: 0, showCounter: true }
 
 // if the state has multiple properties, all of them have to be set in the Reducer because we are returning the overall state object
 
 const counterReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
+  if (action.type === 'increment') {
     // we should NEVER MUTATE THE EXISTING STATE
 
     // state.counter++;
@@ -13,32 +13,32 @@ const counterReducer = (state = initialState, action) => {
 
     return {
       counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
+      showCounter: state.showCounter
+    }
     // always overwrite it by returning a brand new state object in which we MUST ALWAYS SET THE ALL THE OTHER STATES WHEN WE UPDATE A PIECE OF STATE
   }
 
-  if (action.type === "increase") {
+  if (action.type === 'increase') {
     return {
       counter: state.counter + action.amount, // property set in the dispatch function and now the reducer is dynamic
-      showCounter: state.showCounter,
-    };
+      showCounter: state.showCounter
+    }
   }
-  if (action.type === "decrement") {
+  if (action.type === 'decrement') {
     return {
       counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
+      showCounter: state.showCounter
+    }
   }
-  if (action.type === "toggle") {
+  if (action.type === 'toggle') {
     return {
       showCounter: !state.showCounter,
-      counter: state.counter,
-    };
+      counter: state.counter
+    }
   }
-  return state;
-};
+  return state
+}
 
-const store = createStore(counterReducer); // this creates our Redux Store
+const store = createStore(counterReducer) // this creates our Redux Store
 
-export default store; // provide this store to the React app
+export default store // provide this store to the React app
